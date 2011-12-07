3fa9767a69dd67e73b5e949c58f7d465e546be39
Brian Birtles <birtles@gmail.com>
2011-12-07 15:09:36 +0900

    Add stroke size selection

--- js/editor-ui.js
+++ js/editor-ui.js
@@ -3,6 +3,7 @@ var EditorUI = EditorUI || {};
 EditorUI.init = function() {
   var svgRoot = document.getElementById("canvas");
   ParaPara.init(svgRoot);
+  EditorUI.initStrokeWidths();
   EditorUI.initColors();
   EditorUI.updateLayout();
 }
@@ -21,32 +22,36 @@ EditorUI.finish = function() {
   ParaPara.animate(speedAdjust.value);
 }
 
+// -------------- Stroke width -----------
+
+EditorUI.initStrokeWidths = function() {
+  EditorUI.initButtonGroup("strokeWidthButton", EditorUI.changeStrokeWidth, 1);
+}
+
+EditorUI.changeStrokeWidth = function(button) {
+  var button = EditorUI.selectButtonInGroup(button, "strokeWidthButton");
+  if (!button)
+    return;
+  ParaPara.currentStyle.strokeWidth = EditorUI.getStrokeWidthFromButton(button);
+}
+
+EditorUI.getStrokeWidthFromButton = function(elem) {
+  var circles = elem.getElementsByTagName("circle");
+  if (!circles.length)
+    return "4";
+  return parseFloat(circles[0].getAttribute("r")) * 2;
+}
+
 // -------------- Colors -----------
 
 EditorUI.initColors = function() {
-  var buttons = document.getElementsByClassName("colorButton");
-  for (var i = 0; i < buttons.length; i++) {
-    var button = buttons[i];
-    button.addEventListener("click",
-      function(evt) { EditorUI.changeColor(evt.target); }, false);
-  }
-  if (buttons.length) {
-    EditorUI.changeColor(buttons[0]);
-  }
+  EditorUI.initButtonGroup("colorButton", EditorUI.changeColor, 0);
 }
 
 EditorUI.changeColor = function(button) {
-  for (; button && button.tagName != "BUTTON"; button = button.parentNode);
+  var button = EditorUI.selectButtonInGroup(button, "colorButton");
   if (!button)
     return;
-
-  var buttons = document.getElementsByClassName("colorButton");
-  for (var i = 0; i < buttons.length; i++) {
-    buttons[i].classList.remove("active");
-  }
-  button.classList.add("active");
-  button.blur(); // Get rid of outline that appears on tablets
-                 // (unfortunately outline:none doesn't seem to work)
   ParaPara.currentStyle.currentColor = EditorUI.getColorFromButton(button);
 }
 
@@ -55,6 +60,38 @@ EditorUI.getColorFromButton = function(elem) {
   if (!elemsWithAFill.length)
     return "black";
   return elemsWithAFill[0].getAttribute("fill");
+}
+
+// -------------- Common button handling -----------
+
+EditorUI.initButtonGroup = function(className, handler, selectedIndex) {
+  var buttons = document.getElementsByClassName(className);
+  for (var i = 0; i < buttons.length; i++) {
+    var button = buttons[i];
+    button.addEventListener("click",
+      function(evt) { handler(evt.target); }, false);
+  }
+  if (selectedIndex <= buttons.length - 1) {
+    handler(buttons[selectedIndex]);
+  }
+}
+
+EditorUI.selectButtonInGroup = function(evtTarget, className) {
+  var button;
+  for (button = evtTarget;
+       button && button.tagName != "BUTTON";
+       button = button.parentNode);
+  if (!button)
+    return null;
+
+  var buttons = document.getElementsByClassName(className);
+  for (var i = 0; i < buttons.length; i++) {
+    buttons[i].classList.remove("active");
+  }
+  button.classList.add("active");
+  button.blur(); // Get rid of outline that appears on tablets
+                 // (unfortunately outline:none doesn't seem to work)
+  return button;
 }
 
 // -------------- Speed control -----------
@@ -98,6 +135,27 @@ EditorUI.updateLayout = function() {
   } else {
     canvas.style.removeProperty("margin-top");
   }
+  EditorUI.updateStrokeWidth(maxDim);
 }
 window.addEventListener("resize", EditorUI.updateLayout, false);
 window.addEventListener("orientationchange", EditorUI.updateLayout, false);
+
+// As the SVG resizes, keep the stroke width icons in sync
+EditorUI.updateStrokeWidth = function(svgDim) {
+  // The SVG viewBox is 300x300 so get its current scale ratio
+  var svgScaleRatio = svgDim / 300;
+  // Work out the actual dimension of the icon (we might tweak the width,
+  // so use the height)
+  var svgs = document.querySelectorAll("button.strokeWidthButton svg");
+  if (!svgs.length)
+    return;
+  var height = parseFloat(window.getComputedStyle(svgs[0], null).
+                 getPropertyValue('height'));
+  // Go through each button's svg and adjust the viewBox accordingly.
+  var viewBoxSize = height / svgScaleRatio;
+  var minDim = -viewBoxSize / 2;
+  var viewBox = [minDim, minDim, viewBoxSize, viewBoxSize].join(" ");
+  for (var i = 0; i < svgs.length; i++) {
+    svgs[i].setAttribute("viewBox", viewBox);
+  }
+}
