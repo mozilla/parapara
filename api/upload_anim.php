b259698e6f0ad7a3ddd12a2c2833f0cb2a9c5596
Brian Birtles <birtles@gmail.com>
2012-03-12 15:19:16 +0900

    Add URL shortening

--- api/upload_anim.php
+++ api/upload_anim.php
@@ -58,8 +58,7 @@ try {
   } else {
     fwrite($svgfile, $svg);
     fclose($svgfile);
-    $url = rel2abs($svgfilename, curUrl());
-    // XXX Shorten URL
+    $url = shortenUrl(rel2abs($svgfilename, curUrl()));
     print "{\"id\":$id,\"url\":\"$url\"}\n\n";
   }
 } catch (Exception $e) {
@@ -75,30 +74,30 @@ mysql_close($connection);
 // a more thoroughgoing implementation (BSD license).
 function rel2abs($rel, $base)
 {
-  /* return if already absolute URL */
+  // return if already absolute URL 
   if (parse_url($rel, PHP_URL_SCHEME) != '') return $rel;
 
-  /* queries and anchors */
+  // queries and anchors
   if ($rel[0]=='#' || $rel[0]=='?') return $base.$rel;
 
-  /* parse base URL and convert to local variables:
-     $scheme, $host, $path */
+  // parse base URL and convert to local variables:
+  // $scheme, $host, $path
   extract(parse_url($base));
 
-  /* remove non-directory element from path */
+  // remove non-directory element from path
   $path = preg_replace('#/[^/]*$#', '', $path);
 
-  /* destroy path if relative url points to root */
+  // destroy path if relative url points to root
   if ($rel[0] == '/') $path = '';
 
-  /* dirty absolute URL */
+  // dirty absolute URL
   $abs = "$host$path/$rel";
 
-  /* replace '//' or '/./' or '/foo/../' with '/' */
+  // replace '//' or '/./' or '/foo/../' with '/'
   $re = array('#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#');
   for($n=1; $n>0; $abs=preg_replace($re, '/', $abs, -1, $n)) {}
 
-  /* absolute URL is ready! */
+  // absolute URL is ready!
   return $scheme.'://'.$abs;
 }
 
@@ -112,5 +111,29 @@ function curUrl() {
   $url = ($ssl ? 'https://' : 'http://') . $_SERVER["SERVER_NAME"] . $port .
          $_SERVER["SCRIPT_NAME"];
   return $url;
+}
+
+// Based on: http://davidwalsh.name/bitly-api-php
+function shortenUrl($url) {
+  if (!defined('BITLY_LOGIN') || !defined('BITLY_API_KEY'))
+    return $url;
+  $shorten_url = "http://api.bit.ly/v3/shorten?login=" . BITLY_LOGIN .
+    "&apiKey=" . BITLY_API_KEY . "&uri=" . urlencode($url) . "&format=txt";
+  $shortened = curl_get_result($shorten_url);
+  // If there is an error, return the original url
+  return $shortened ? $shortened : $url;
+}
+
+function curl_get_result($url) {
+  $ch = curl_init();
+  $timeout = 5;
+  curl_setopt($ch,CURLOPT_URL,$url);
+  curl_setopt($ch,CURLOPT_RETURNTRANSFER,1);
+  curl_setopt($ch,CURLOPT_CONNECTTIMEOUT,$timeout);
+  $data = curl_exec($ch);
+  // Check status code
+  $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+  curl_close($ch);
+  return $httpCode == 200 ? $data : null;
 }
 ?>
