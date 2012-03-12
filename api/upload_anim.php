cf3e320862745085efd2d226cb6f50ea802b0c57
Brian Birtles <birtles@gmail.com>
2012-03-12 14:46:27 +0900

    Return URL when uploading animation

--- api/upload_anim.php
+++ api/upload_anim.php
@@ -50,8 +50,7 @@ try {
   // XXXbb We should use a DB transaction for the above and roll it back if 
   // saving the file fails
   // (First, switch to PDO?)
-  // XXX Put this path in the config file
-  $svgfilename = "../characters/".$id.".svg";
+  $svgfilename = CHARACTERS_DIR . $id . ".svg";
   $svgfile = @fopen($svgfilename, 'w');
   if ($svgfile == false) {
     print '{"error_key":"failed_to_write",'
@@ -59,13 +58,59 @@ try {
   } else {
     fwrite($svgfile, $svg);
     fclose($svgfile);
-    // XXX Generate URL
+    $url = rel2abs($svgfilename, curUrl());
     // XXX Shorten URL
-    print "{\"id\":$id}\n\n";
+    print "{\"id\":$id,\"url\":\"$url\"}\n\n";
   }
 } catch (Exception $e) {
   $message = $e->getMessage();
   print "{\"error_key\":\"db_error\",\"error_detail\":\"$message\"}\n\n";
 }
 mysql_close($connection);
+
+// Source:
+//  http://nashruddin.com/PHP_Script_for_Converting_Relative_to_Absolute_URL
+//
+// If this proves inadequate, see: http://publicmind.in/blog/urltoabsolute/ for 
+// a more thoroughgoing implementation (BSD license).
+function rel2abs($rel, $base)
+{
+  /* return if already absolute URL */
+  if (parse_url($rel, PHP_URL_SCHEME) != '') return $rel;
+
+  /* queries and anchors */
+  if ($rel[0]=='#' || $rel[0]=='?') return $base.$rel;
+
+  /* parse base URL and convert to local variables:
+     $scheme, $host, $path */
+  extract(parse_url($base));
+
+  /* remove non-directory element from path */
+  $path = preg_replace('#/[^/]*$#', '', $path);
+
+  /* destroy path if relative url points to root */
+  if ($rel[0] == '/') $path = '';
+
+  /* dirty absolute URL */
+  $abs = "$host$path/$rel";
+
+  /* replace '//' or '/./' or '/foo/../' with '/' */
+  $re = array('#(/\.?/)#', '#/(?!\.\.)[^/]+/\.\./#');
+  for($n=1; $n>0; $abs=preg_replace($re, '/', $abs, -1, $n)) {}
+
+  /* absolute URL is ready! */
+  return $scheme.'://'.$abs;
+}
+
+// Based on: http://webcheatsheet.com/php/get_current_page_url.php
+function curUrl() {
+  $ssl  = isset($_SERVER["HTTPS"]) && $_SERVER["HTTPS"] == "on";
+  $port = isset($_SERVER["SERVER_PORT"]) &&
+          ((!$ssl && $_SERVER["SERVER_PORT"] != "80") ||
+           ($ssl  && $_SERVER["SERVER_PORT"] != "443"));
+  $port = $port ? ':' . $_SERVER["SERVER_PORT"] : '';
+  $url = ($ssl ? 'https://' : 'http://') . $_SERVER["SERVER_NAME"] . $port .
+         $_SERVER["SCRIPT_NAME"];
+  return $url;
+}
 ?>
