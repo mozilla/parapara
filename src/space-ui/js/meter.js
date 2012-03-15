0004fa1b3a4ef3f7edd00c631b9eebb85cbeba0d
Brian Birtles <birtles@gmail.com>
2012-03-15 16:27:00 +0900

    Add MPL 2.0 headers to space UI

--- src/space-ui/js/meter.js
+++ src/space-ui/js/meter.js
@@ -1,3 +1,7 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
 // Javascript to drive the meter.svg file.
 //
 // We'd like to embed this in the SVG file itself but Fennec (version 9-ish)
