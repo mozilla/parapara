9c71e1745f0b8051e478cfd49b133199b1eee84d
Brian Birtles <birtles@gmail.com>
2012-03-19 15:11:18 +0900

    Fix paths in filter.php

--- wall/public/characters/filter.php
+++ wall/public/characters/filter.php
@@ -3,8 +3,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-require_once("../../lib/CONSTANTS.inc");
-require_once("../../lib/db.inc");
+require_once("../../lib/parapara.inc");
+require_once("db.inc");
 $connection = getConnection();
 
 if (isset($_REQUEST['action']) && $_REQUEST['action'] == 'update') {
