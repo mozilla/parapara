89f8addf1923ccb45c97f9b0840d103df3e75201
Brian Birtles <birtles@gmail.com>
2012-05-24 17:29:11 +0900

    Add placeholder files

---
+++ wall/public/wall/chirashi.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!-- This Source Code Form is subject to the terms of the Mozilla Public
+   - License, v. 2.0. If a copy of the MPL was not distributed with this file,
+   - You can obtain one at http://mozilla.org/MPL/2.0/.  -->
+<html lang="ja">
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>エベント名</title>
+</head>
+<body>
+  <ul>
+    <li>エベント名</li>
+    <li>壁のURL</li>
+    <li>エディターURL</li>
+    <li>エディターURLのＱＲコード</li>
+    <li>パラパラ・モジラ</li>
+    <li>絵？描いてもらう？</li>
+    <li>印刷するボタン（印刷のスタイルシートで印刷するとき、ボタンを隠す）</li>
+  </ul>
+</body>
+</html>
