7e52f3c1b94d0c5f4ff32d4951728b6a3bd1489d
Daisuke Akatsuka <dadaaism@gmail.com>
2012-06-06 17:21:39 +0900

    wall 4 live

---
+++ wall/public/wall/js/database4gallery.js
@@ -0,0 +1,83 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var API_DIR = "../api/";
+var CHARACTERS_DIR = "../characters/";
+
+var Database = {
+
+  start: function(timebase, characterListener) {
+    Database.characters = [];
+    Database.listener = characterListener;
+    Database.timebase = timebase;
+    Database.timebase.addEventListener("repeatEvent", function(e) {
+      for (var i = 0, n = Database.characters.length; i < n; i++) {
+        Database.characters[i].sent = false;
+      }
+    }, true);
+
+    //changes animate duration. [dur]
+    Utility.applyDuration(Database.timebase, BASE_TIME);
+
+    Database.loadAllCharacters();
+  },
+
+  idle: function() {
+    var currentActiveTime = Main.timebase.getCurrentTime();
+    var simpleDuration = Main.timebase.getSimpleDuration();
+    // Get the time within the current repeat iteration
+    var currentSimpleTime = currentActiveTime % simpleDuration;
+    // Get simple time as a ratio of the simple duration (i.e. how far are we
+    // through the current iteration)
+    var currentRate = currentSimpleTime/simpleDuration;
+    Database.current_rate = currentRate;
+    
+    // Go through and add waiting characters
+    for (var i = 0, n = Database.characters.length; i < n; i++) {
+      var character = Database.characters[i];
+      // If the character is not ready to appear or has already been added
+      // skip it
+      
+      var rate = character.x;
+      if (rate < currentRate && !character.sent) {
+        Database.listener(character, currentActiveTime, currentSimpleTime, currentRate);
+        character.sent = true;
+      }
+    }
+    
+    Database.timeout_id = setTimeout(Database.idle, 100);
+  },
+  
+  // Get all characters that have already been assigned an x value (i.e. have
+  // already made their debut on the stage)
+  loadAllCharacters: function(callback) {
+    var url = API_DIR+"get_all_characters.php?threshold="+
+              NUM_CHARACTERS_THRESHOLD+"&"+(new Date()).getTime();
+    $.getJSON(url, function(json) {
+      Database.append(json);
+      Database.idle();
+    });
+  },
+
+  append: function(json) {
+    for (var i = 0, n = json.length; i < n; i++) {
+      var characterOfJson = json[i];
+      var character = new Character();
+      character.setup(characterOfJson);
+      Database.characters.push(character);
+    }
+  }
+}
+
+function Character() {
+}
+
+Character.prototype = {
+  setup: function(json) {
+    this.x = parseInt(json.x)/1000;
+    this.id = json.id;
+    this.sent = false;
+    this.uri = CHARACTERS_DIR+this.id+".svg";
+  }
+}
---
+++ wall/public/wall/js/utility.js
@@ -0,0 +1,54 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this file,
+ * You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+var Utility = {
+  applyDuration: function(timebaseElement, basetime, begintime) {
+    var baseduration = Utility.toMilliseconds(Database.timebase.getAttribute("dur"));
+    var durationRate = basetime/baseduration;
+    Utility.applyDuration2Element(timebaseElement, durationRate, basetime, begintime);
+    var animations = $("animateTransform");
+    for (var i = 0, n = animations.length; i < n; i++) {
+      var animation = animations[i];
+      if (animation == timebaseElement) {
+        continue;
+      }
+      Utility.applyDuration2Element(animation, durationRate, basetime, begintime);
+    }
+  },
+  
+  applyDuration2Element: function(element, durationRate, baseTime, beginTime) {
+    var durationMilliseconds = Utility.toMilliseconds(element.getAttribute("dur"));
+    if (durationMilliseconds) {
+      var duration = Math.round(durationMilliseconds) * durationRate;
+      element.setAttribute("dur", duration+"ms");
+    }
+    var beginAttribute = element.getAttribute("begin");
+    var begin4delay = -beginTime;
+//    var begin4delay = -Math.round(beginTime*(baseTime/durationMilliseconds));
+    if (beginAttribute) {
+      var beginMilliseconds = Utility.toMilliseconds(beginAttribute);
+      if (beginMilliseconds) {
+        var begin = Math.round(beginMilliseconds) * durationRate;
+        element.setAttribute("begin", (begin+begin4delay)+"ms");
+      }
+    } else {
+      element.setAttribute("begin", begin4delay+"ms");
+    }
+    var endAttribute = element.getAttribute("end");
+    if (endAttribute) {
+      var endMilliseconds = Utility.toMilliseconds(endAttribute);
+      if (endMilliseconds) {
+        var end = Math.round(endMilliseconds) * durationRate;
+        element.setAttribute("end", end+"ms");
+      }
+    }
+  },
+  
+  toMilliseconds: function(string) {
+    var matches = string.match(/(\d+)(m?s)/);
+    var value = parseInt(matches[1]);
+    var unit = matches[2];
+    return value * (unit == "s" ? 1000 : 1);
+  }
+}
