26021598ed8748195320bcb0d26cf24650772bc5
dadaa <dadaa@mz-community.naist.jp>
2012-06-28 17:05:48 +0900

    chmod

--- wall/public/api/upload_anim.php
+++ wall/public/api/upload_anim.php
@@ -15,9 +15,6 @@ header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header('Access-Control-Allow-Headers: content-type');
 header('Access-Control-Max-Age: 86400');
 
-if ($_SERVER['REQUEST_METHOD'] == 'OPTIONS')
-  exit;
-
 header("Content-Type: text/plain; charset=UTF-8");
 
 // Parse wall name
