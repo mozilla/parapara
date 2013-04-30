d4e6d0a7e9c4edb7117369ce415ca1acd58afb3e
Brian Birtles <birtles@gmail.com>
2013-04-30 16:29:38 +0900

    Slightly tweak character list

--- wall/public/characters/index.php
+++ wall/public/characters/index.php
@@ -15,18 +15,17 @@ $connection = getConnection();
   </head>
 <body>
 <table cellpadding="5" style="font-size: 1.2em">
-<tr><th>&nbsp;</td><th>名前</th><th>タイトル</th><th>URL</th></tr>
+<tr><th>名前</th><th>タイトル</th><th>URL</th></tr>
 <?php
   $self = $_SERVER['PHP_SELF'];
   $base_url = $_SERVER['HTTPS'] == 'on' ? "https://" : "http://";
   $base_url .= $_SERVER['HTTP_HOST'];
   $base_url .= substr($self, 0, strrpos($self, '/') + 1);
-  $query = "SELECT charId, author, title, active FROM characters";
+  $query = "SELECT charId, author, title FROM characters WHERE active = TRUE";
   $result = mysql_query($query, $connection) or throwException(mysql_error());
   while ($row = mysql_fetch_array($result, MYSQL_NUM)) {
     $url = $base_url . $row[0] . ".svg";
     echo "<tr>";
-    echo "<td>" . ($row[3] ? "&#x25cb;" : "&nbsp;") . "</td>";
     echo "<td>" . $row[1] . "</td>";
     echo "<td>" . $row[2] . "</td>";
     echo "<td><a href=\"$url\">$url</a></td>";
