b78eb2d486e86785a25cc1687afe566f5e5008df
Brian Birtles <birtles@gmail.com>
2013-05-01 13:55:09 +0900

    Get character information from file when displaying gallery view
    This allows us to delete the record and keep the file

--- wall/public/characters/gallery.php
+++ wall/public/characters/gallery.php
@@ -3,115 +3,136 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this file,
  * You can obtain one at http://mozilla.org/MPL/2.0/. 
  */
-date_default_timezone_set('Asia/Tokyo');
-$id = intval($_GET["id"]);
+
 require_once("../../lib/parapara.inc");
 require_once("db.inc");
-$connection = getConnection();
-try {
-	$query = "SELECT title,author,rtime FROM characters WHERE id=$id";
-	$result = mysql_query($query, $connection) or throwException(mysql_error());
-	if ($row = mysql_fetch_array($result)) {
-		$author = $row["author"];
-		$title = $row["title"];
-		$rtime = $row["rtime"];
-		$createdtime = date('Y-m-d H:i:s', intval($rtime)/1000);
-	} else {
-		$error = "Not found";
-	}
-	mysql_free_result($result);
-} catch (Exception $e) {
-	$error = $e->getMessage();
+require_once("characters.inc");
+require_once("walls.inc");
+
+// Check for a valid ID
+$id = intval($_REQUEST['id']);
+if ($id < 1 || !@file_exists($id . '.svg')) {
+  $filename = "sad-face.svg";
+} else {
+  // Load the SVG
+  $filename = $id . '.svg';
+  $doc = new DOMDocument();
+  $doc->load($filename);
+
+  // Parse title and description
+  $xpath = new DOMXPath($doc);
+  $xpath->registerNamespace('svg', 'http://www.w3.org/2000/svg');
+  $titleNodes = $xpath->query('svg:title');
+  $title = $titleNodes ? $titleNodes->item(0)->textContent : null;
+  $descNodes = $xpath->query('svg:desc');
+  $desc = $descNodes ? $descNodes->item(0)->textContent : null;
+
+  // Try to get the event details
+  try {
+    $char = Characters::getById($id);
+    if ($char) {
+      if ($char->createDate) {
+        $createDate = strtotime($char->createDate);
+      }
+      $wall = Walls::getById($char->wallId);
+    }
+    if ($wall) {
+      $eventName = $wall->name;
+    }
+  } catch (KeyedException $e) {
+  }
 }
-mysql_close($connection);
 ?>
 <!DOCTYPE html>
-<html lang="ja" xmlns="http://www.w3.org/1999/xhtml">
+<html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-	<meta charset="UTF-8">
-	<title>Parapara Animation</title>
-	<link rel="icon" type="image/png" href="favicon.png">
-	<style type="text/css">
-	body {
-		font-family: helvetica;
-		color: #333;
-	}
-	#content {
-		width: 600px;
-		margin: 0 auto;
-	}
-	#title {
-		font-size: 24px;
-		margin: 18px 0px 12px 0px;
-		text-align: center;
-	}
-	#description {
-		margin-top: 3px;
-		text-align: center;
-		font-size: 12px;
-	}
-	#date {
-		display: inline-block;
-	}
-	#place {
-		display: inline-block;
-	}
-	#parapara {
-		width: 600px;
-		height: 380px;
-		border-radius: 10px;
-		-moz-border-radius: 10px;
-		-webkit-border-radius: 10px;
-		-o-border-radius: 10px;
-		-ms-border-radius: 10px;
-	}
-	.fb-like {
-		position: relative;
-		top: -4px;
-	}
-	#character {
-		background-color: black;
-		border-radius: 20px;
-		-moz-border-radius: 20px;
-		-webkit-border-radius: 20px;
-		-o-border-radius: 20px;
-		-ms-border-radius: 20px;
-	}
-	#message {
-		font-size: 10px;
-		margin-top: 10px;
-	}
-	#feedbacks {
-		margin-top: 10px;
-		text-align: center;
-	}
-	</style>
+  <meta charset="UTF-8">
+  <title>Parapara Animation</title>
+  <link rel="icon" type="image/png" href="favicon.png">
+  <style type="text/css">
+  body {
+    font-family: helvetica;
+    color: #333;
+  }
+  #content {
+    width: 600px;
+    margin: 0 auto;
+  }
+  #title {
+    font-size: 24px;
+    margin: 18px 0px 12px 0px;
+    text-align: center;
+  }
+  #description {
+    margin-top: 3px;
+    text-align: center;
+    font-size: 12px;
+  }
+  #event, #date {
+    display: inline-block;
+  }
+  #parapara {
+    width: 600px;
+    height: 380px;
+    border-radius: 10px;
+    -moz-border-radius: 10px;
+    -webkit-border-radius: 10px;
+    -o-border-radius: 10px;
+    -ms-border-radius: 10px;
+  }
+  .fb-like {
+    position: relative;
+    top: -4px;
+  }
+  #character {
+    background-color: black;
+    border-radius: 20px;
+    -moz-border-radius: 20px;
+    -webkit-border-radius: 20px;
+    -o-border-radius: 20px;
+    -ms-border-radius: 20px;
+  }
+  #message {
+    font-size: 10px;
+    margin-top: 10px;
+  }
+  #feedbacks {
+    margin-top: 10px;
+    text-align: center;
+  }
+  </style>
 </head>
 <body>
-	<div id="content">
-    <div id="title"><?php
-    echo $title;
-    if ($title && $author)
-      echo " - ";
-    if ($author)
-      echo $author . "さん作";
-    ?></div>
-		<center>
-		<img id="character" width="400" height="400" src="<?php print $id; ?>.svg">
-		</center>
-		<div id="description">
-			<div id="date"><?php print $createdtime; ?></div>
-			<div id="place">Mozilla ワークショップ in 福島</div>
-		</div>
+  <div id="content"><?php
+if ($title || $desc) {
+  echo "\n<div id=\"title\">\n";
+  $elems = array();
+  if (!empty($title)) array_push($elems, $title);
+  if (!empty($desc)) array_push($elems, $desc);
+  echo implode($elems, " - ");
+  echo "\n</div>\n";
+} ?>
+    <center>
+    <img id="character" width="400" height="400" src="<?php echo $filename; ?>">
+    </center>
+<?php if (!empty($eventName)): ?>
+    <div id="description">
+      <div id="event"><?php echo $eventName ?></div>
+<?php if (!empty($eventName) && !empty($createDate)) echo " / "; ?>
+<?php if (!empty($createDate)): ?>
+      <div id="date"><?php echo date("Y-m-d", $createDate) ?></div>
+<?php endif; ?>
+    </div>
+<?php endif; ?>
 
-		<div id="feedbacks">
-			<div class="fb-like" data-href="http://parapara.mozlabs.jp/Fukushima100/characters/<?php print $id; ?>" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>		
-			<a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
-		</div>		
+    <div id="feedbacks">
+      <div class="fb-like" data-href="http://parapara.mozlabs.jp/Fukushima100/characters/<?php echo $id; ?>" data-send="false" data-layout="button_count" data-width="100" data-show-faces="false"></div>   
+      <a href="https://twitter.com/share" class="twitter-share-button">Tweet</a>
+    </div>    
 
-		<div id="message">
-		</div>		
-		
+    <div id="message">
+    </div>    
+    
 <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
 <div id="fb-root"></div>
 <script>(function(d, s, id) {
@@ -122,6 +143,6 @@ mysql_close($connection);
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
 
-	</div>
+  </div>
 </body>
 </html>
