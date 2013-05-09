12ad9e64f0cda3c27baeeaff1b146e1ff218d147
Brian Birtles <birtles@gmail.com>
2013-05-09 16:17:09 +0900

    Fill in CharacterEmailer

--- wall/tests/unit/TestCharacterEmail.php
+++ wall/tests/unit/TestCharacterEmail.php
@@ -9,6 +9,9 @@ require_once('characters.inc');
 require_once('characters.email.inc');
 
 class TestCharacterEmail extends ParaparaUnitTestCase {
+  protected $testMailer  = null;
+  protected $testAddress = "test@test.org";
+
   function __construct($name = false) {
     parent::__construct($name);
   }
@@ -16,13 +19,68 @@ class TestCharacterEmail extends ParaparaUnitTestCase {
   function setUp() {
     parent::setUp();
 
+    $this->testMailer =& Mail::factory('mock');
+
+    // Make sure a transport is set initially
     global $config;
     $config['mail']['transport'] = 'mock';
   }
 
+  function testEnabled() {
+    // Remove mail configuration in settings
+    global $config;
+    unset($config['mail']['transport']);
+    $this->assertFalse(CharacterEmailer::isEmailEnabled());
+    try {
+      CharacterEmailer::sendEmail($this->testCharacter, $this->testAddress);
+      $this->fail("Failed to throw exception with no mail transport");
+    } catch (KeyedException $e) {
+      $this->assertEqual($e->getKey(), "email-disabled");
+    }
+
+    // Supply mailer manually
+    try {
+      CharacterEmailer::sendEmail($this->testCharacter, $this->testAddress,
+        null, $this->testMailer);
+    } catch (KeyedException $e) {
+      $this->fail("Failed to send mail with manual mailer, exception: "
+                  . $e->getKey());
+    }
+  }
+
   function testAddress() {
+    try {
+      CharacterEmailer::sendEmail($this->testCharacter, "");
+      $this->fail("Failed to throw exception with no email address");
+    } catch (KeyedException $e) {
+      $this->assertEqual($e->getKey(), "bad-email");
+    }
   }
 
   function testSend() {
+    global $config;
+
+    // Send
+    CharacterEmailer::sendEmail($this->testCharacter, $this->testAddress,
+                                null, $this->testMailer);
+
+    // Check message was sent
+    $this->assertEqual(count($this->testMailer->sentMessages), 1);
+    $message = @$this->testMailer->sentMessages[0];
+
+    // Check address
+    $this->assertEqual(@$message['recipients'], $this->testAddress);
+
+    // Check headers
+    $this->assertEqual(@$message['headers']['From'], $config['mail']['from']);
+
+    // Check body
+    $this->assertTrue(strlen(@$message['body']) > 0);
+    $this->assertTrue(
+      strpos(@$message['body'], $this->testCharacter->galleryUrl) !== false);
+    $this->assertTrue(
+      strpos(@$message['body'], $this->testCharacter->title) !== false);
+    $this->assertTrue(
+      strpos(@$message['body'], $this->testCharacter->author) !== false);
   }
 }
