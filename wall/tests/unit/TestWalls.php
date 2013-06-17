53077ad58777ebb532a7bb76c578a02b92c7c442
Brian Birtles <birtles@gmail.com>
2013-06-17 11:47:34 +0900

    Add method to get sessions and their corresponding characters

--- wall/tests/unit/TestWalls.php
+++ wall/tests/unit/TestWalls.php
@@ -18,7 +18,59 @@ class TestWalls extends ParaparaUnitTestCase {
   }
 
   function testGetSessionAndCharacters() {
-    // TBD
+    // Test structure
+    //   - Session 1
+    //     - Character A
+    //     - Character B
+    //   - Session 2
+    //   - Session 3
+    //     - Character C
+
+    $fields = $this->testCharacterFields;
+
+    // Session 1
+    $session1 = $this->testWall->latestSession;
+    $fields['title'] = 'Character A';
+    $charA = $this->createCharacter($fields);
+    $fields['title'] = 'Character B';
+    $charB = $this->createCharacter($fields);
+
+    // Session 2
+    $this->testWall->startSession($session1['sessionId']);
+    $session2 = $this->testWall->latestSession;
+
+    // Session 3
+    $this->testWall->startSession($session2['sessionId']);
+    $session3 = $this->testWall->latestSession;
+    $fields['title'] = 'Character C';
+    $charC = $this->createCharacter($fields);
+
+    // Get summary
+    $summary = $this->testWall->getSessions("Include characters");
+
+    // Test structure
+    $this->assertEqual(count($summary), 3);
+
+    // Test session 1
+    $this->assertIdentical($summary[0]['sessionId'], $session1['sessionId']);
+    $this->assertTrue(array_key_exists('characters', $summary[0]),
+                      "No characters array along with session");
+    $this->assertEqual(count(@$summary[0]['characters']), 2);
+    $this->assertIdentical(@$summary[0]['characters'][0], $charA);
+    $this->assertIdentical(@$summary[0]['characters'][1], $charB);
+
+    // Test session 2
+    $this->assertIdentical($summary[1]['sessionId'], $session2['sessionId']);
+    $this->assertTrue(array_key_exists('characters', $summary[1]),
+                      "No characters array along with session");
+    $this->assertEqual(count(@$summary[1]['characters']), 0);
+
+    // Test session 3
+    $this->assertIdentical($summary[2]['sessionId'], $session3['sessionId']);
+    $this->assertTrue(array_key_exists('characters', $summary[2]),
+                      "No characters array along with session");
+    $this->assertEqual(count(@$summary[2]['characters']), 1);
+    $this->assertIdentical(@$summary[2]['characters'][0], $charC);
   }
 }
 
