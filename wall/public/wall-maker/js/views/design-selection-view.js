d6247094dee5f509ba45f0425388c35fca637662
Brian Birtles <birtles@gmail.com>
2013-07-11 17:14:54 +0900

    Add selection handling to design selection
    Switch on video and update label

--- wall/public/wall-maker/js/views/design-selection-view.js
+++ wall/public/wall-maker/js/views/design-selection-view.js
@@ -8,9 +8,36 @@ define([ 'underscore',
          'text!templates/design-selection.html' ],
 function(_, Backbone, BaseView, template) {
   return BaseView.extend({
+    events: {
+      "change input[type=radio]": "radioChange"
+    },
     render: function() {
       this.renderTemplate(template, { designs: this.collection.toJSON() });
       return this;
+    },
+    radioChange: function(evt, reset) {
+      this.$("input[type=radio]").each(function(index, radio) {
+        var selected = this.checked && !reset;
+
+        // Set selected class on parent label
+        var label = $(this).parent("label");
+        if (!label)
+          return;
+        label.toggleClass("selected", selected);
+
+        // Update play state of video
+        var videos = label.find("video");
+        if (videos.length) {
+          if (selected) {
+            videos[0].play();
+          } else {
+            videos[0].pause();
+            try {
+              videos[0].currentTime = 0;
+            } catch (e) { /* Ignore bogus exceptions from WebKit */ }
+          }
+        }
+      });
     }
   });
 });
