fdff5fd743406e6adeec974e2788939567771416
Brian Birtles <birtles@gmail.com>
2013-07-16 17:18:15 +0900

    Add basic wall saving

--- wall/public/wall-maker/js/views/new-wall-screen-view.js
+++ wall/public/wall-maker/js/views/new-wall-screen-view.js
@@ -12,11 +12,15 @@ function(_, Backbone, webL10n, BaseView, DesignSelectionView, template) {
   return BaseView.extend({
     el: $("#screen-new"),
     events: {
+      "click button[type=submit]": "create",
       "click button.cancel": "cancel"
     },
     initialize: function() {
       this.designSelectionView =
         new DesignSelectionView({ collection: this.options.designs });
+      // Add a 'form' property
+      Object.defineProperty(this, "form",
+        { get: function() { return this.$("form")[0]; }, enumerable: true });
     },
     render: function() {
       // There are no variables in the template for this view so far so we can
@@ -29,9 +33,34 @@ function(_, Backbone, webL10n, BaseView, DesignSelectionView, template) {
 
       return this;
     },
+    create: function(evt) {
+      // Don't actually submit the form
+      evt.preventDefault ? evt.preventDefault() : evt.returnValue = false;
+
+      // Get values
+      name = this.form.name.value;
+      design = parseInt($("input[name=design]:checked", this.form).val());
+
+      // XXX Clear message box
+      // XXX Show loading screen / indication
+
+      // Create wall
+      var newWallView = this;
+      this.options.walls.create({ name: name, design: design },
+        { wait: true,
+          success: function(wall) {
+            // XXX Redirect to manage wall page
+            newWallView.form.reset();
+          },
+          error: function() {
+            // XXX Display error message here
+          }
+        });
+    },
     cancel: function() {
       Backbone.history.navigate('./', { trigger: true });
-      this.$("form").trigger("reset");
+      this.form.reset();
+      // XXX Reset the message box too
     }
   });
 });
