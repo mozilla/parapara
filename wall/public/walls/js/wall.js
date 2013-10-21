b4649dac96a71b0c7c438a34fa1c3500281e5dce
Brian Birtles <birtles@gmail.com>
2013-10-21 18:30:40 +0200

    Start working around Safari bugs

--- wall/public/walls/js/wall.js
+++ wall/public/walls/js/wall.js
@@ -76,8 +76,10 @@ function ($) {
             container =
               wall.doc.getElementById(
                 characterElem.getAttribute("data-target"));
-            if (container)
+            if (container) {
               container.appendChild(characterElem);
+              // wall.kickStartSafari(characterElem);
+            }
           }
 
           elems.push(characterElem);
@@ -244,6 +246,8 @@ function ($) {
     // Very simple time parsing: Only supports 's' and 'ms' times.
     // Returns a value in milliseconds
     parseTime: function(str) {
+      if (!str)
+        return null;
       var matches = str.match(/^\s*(-?[0-9.]+)(m?s)\s*$/);
       return matches
         ? parseFloat(matches[1]) * (matches[2] == "s" ? 1000 : 1)
@@ -272,6 +276,7 @@ function ($) {
         // Put this animation in the same place
         anim.parentNode.insertBefore(clone, anim);
         anim.parentNode.removeChild(anim);
+        // XXX Need to kick-start Safari
 
         return clone;
       }
@@ -316,8 +321,8 @@ function ($) {
 
       // Clone children--generally animation elements don't have interesting
       // children so we can just clone them blindly
-      for (var i = 0; i < elem.children.length; i++) {
-        clone.appendChild(elem.children[i].cloneNode());
+      for (var i = 0; i < elem.childNodes.length; i++) {
+        clone.appendChild(elem.childNodes[i].cloneNode());
       }
       return clone;
     },
@@ -388,6 +393,31 @@ function ($) {
       }
       return elem;
     },
+
+    // Safari has a bug where animations added dynamically don't get started so
+    // we have to kick-start them
+    kickStartSafari: function(instance) {
+      console.assert(instance.ownerDocument,
+        "Instance should be attached before this is called");
+      var animations =
+        instance.querySelectorAll("animate, animateMotion, animateTransform"
+                                  + ", set");
+      var wall = this;
+      Array.prototype.forEach.call(animations,
+        function(animation) {
+          try {
+            if (animation.getStartTime() === Infinity) {
+              var startTime =
+                wall.parseTime(animation.getAttribute("begin")) / 1000;
+              if (startTime === null)
+                startTime = 0;
+              // XXX We might only need a call to beginElement here
+              animation.beginElementAt(
+                startTime - animation.ownerSVGElement.getCurrentTime());
+            }
+          } catch (e) { }
+        });
+    }
   });
 
   // Helpers
