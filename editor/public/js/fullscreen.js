9857cc83e4427a7e272dfcb1b6e4cfd115a8ed49
shundroid <ra24483@qb3.so-net.ne.jp>
2015-05-24 10:21:37 +0900

    Fixed2 #30

---
+++ editor/public/js/fullscreen.js
@@ -0,0 +1,24 @@
+document.addEventListener("DOMContentLoaded", function() {
+	document.getElementById("burgerbutton").addEventListener("click", function() {
+		if ($('div.tool-box').css('display') === "none") {
+			showTool();
+		} else {
+			hideTool();
+		}
+	});
+	document.getElementById("burgerbutton").addEventListener("mousemove", function(e) {
+		if (e.buttons === 1) {
+			$('#burgerbutton').css("left", e.clientX - 25).css("top", e.clientY - 25);
+		}
+	});
+});
+
+function hideTool() {
+	$('div.tool-box').css("display", "none");
+	$('div.film-strip').css("display", "none");
+}
+
+function showTool() {
+	$('div.tool-box').css("display", "block");
+	$('div.film-strip').css("display","block");
+}
